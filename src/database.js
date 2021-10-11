a3f04daf4046eb173185dbda0863343ab7b1625a
Robert Álefe <alefetorresrr@gmail.com>
2021-10-10 22:38:56 -0400

    neo4j intro

--- src/database.js
+++ src/database.js
@@ -1,3 +1,5 @@
+//MONGODB
+
 const mongoose = require('mongoose');
 
 const MONGODB_URI = process.env.MONGOURL;
@@ -9,3 +11,26 @@ mongoose.connect(MONGODB_URI, {
 .then(db => console.log('Database is connected to:',db.connection.host))
 .catch(err => console.log(err));
 
+//NEO4J
+
+const neo4j = require('neo4j-driver');
+const driver = new neo4j.driver("neo4j://localhost:7687", neo4j.auth.basic("neo4j","1234"));
+
+const session = driver.session({
+    database: "neo4j",
+});
+
+console.log("Conetado ao Neo4J");
+
+const neo = async () => {
+    const nodes = []
+    const results = await session.run("MATCH (n: Pessoa) RETURN (n)",{});
+    results.records.forEach(res => {
+        const properties = res.get(0).properties;
+        nodes.push({nome: properties.nome, idade: properties.idade.low })
+    });
+    console.log(nodes);
+    session.close();
+}
+
+neo();
